4e773b86c137ff473911013d2eb8e42e197bdb76
HoengLyhout-web <lyhouthoeng@gmail.com>
2026-01-21 21:28:42 +0700

    Add files via upload

---
+++ MyfirstWeb.js
@@ -0,0 +1,5 @@
+const btn = document.getElementById("clickBtn");
+
+btn.addEventListener("click", function() {
+       alert("🎉Congrat!!! You are now at my website my lov-, I mean my friends haha😅");
+});
